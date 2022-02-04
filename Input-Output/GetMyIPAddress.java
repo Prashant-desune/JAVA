cfb9e810ad230723c1c61fae722ad4432f174140
Prashant Kumar <prashant.desune@gmail.com>
2022-02-04 16:37:26 +0530

    Add files via upload

---
+++ Input-Output/GetMyIPAddress.java
@@ -0,0 +1,12 @@
+//Find the API 
+import java.net.InetAddress;
+
+class GetMyIPAddress
+{
+   public static void main(String args[]) throws Exception
+   {
+      InetAddress myIP=InetAddress.getLocalHost();
+      System.out.println("My IP Address is:");
+      System.out.println(myIP.getHostAddress());
+  }
+}
