f20d37cae9071832edde904ab2deed4ec212476f
Prashant Kumar <prashant.desune@gmail.com>
2022-02-06 15:28:56 +0530

    Add files via upload

---
+++ Type Conversion/ConverString.java
@@ -0,0 +1,8 @@
+//Java Program to convert string type variables into boolean
+public class ConverString {
+    public static void main(String[] args) {
+        String a  = "true";
+        boolean b = Boolean.valueOf(a);
+        System.out.println("The Value is : " + a);
+    }
+}
