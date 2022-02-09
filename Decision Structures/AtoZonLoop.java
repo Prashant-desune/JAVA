693ca4e6de9ea13100de8abbb12cce320e8f3b08
Prashant Kumar <prashant.desune@gmail.com>
2022-02-09 08:18:02 +0530

    Add files via upload

---
+++ Decision Structures/AtoZonLoop.java
@@ -0,0 +1,11 @@
+//
+import java.util.*;
+public class AtoZonLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c;
+        for(c='a';c<='z';++c){
+            System.out.print ("  "+c);
+        }
+    }
+}
