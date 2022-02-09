814d4c0f8b41dc7f32e9caa87474a9191f3647cb
Prashant Kumar <prashant.desune@gmail.com>
2022-02-09 08:18:21 +0530

    Add files via upload

---
+++ Decision Structures/DescendingOrderNames.java
@@ -0,0 +1,9 @@
+/*Write a program that prompts the user to enter three names. 
+Your program should display the names in descending order.*/
+
+import java.util.*;
+public class DescendingOrderNames {
+    public static void main(String[] args) {
+        
+    }
+}
