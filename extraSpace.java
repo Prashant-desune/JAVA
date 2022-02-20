1b0fdce208948af1c035c96198324e4c06b5fbc5
Prashant Kumar <prashant.desune@gmail.com>
2022-02-20 11:35:49 +0530

    Delete extraSpace.java

--- extraSpace.java
+++
@@ -1,8 +0,0 @@
-//extra Space...
-public class extraSpace {
-    public static void main(String[] args) {
-        String str = "This is me 0abc9";
-        String Space = str.replaceAll("\\s","");
-        System.out.println(Space);
-    }
-}
