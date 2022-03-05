3935302e6a43520ac4f8e16a18ea39442c9411c6
Prashant Kumar <prashant.desune@gmail.com>
2022-03-06 01:07:20 +0530

    Add files via upload

---
+++ Arrays/PrintFiveRollNo.java
@@ -0,0 +1,8 @@
+public class PrintFiveRollNo {
+    public static void main(String[] args) {
+        int num[] = new int[5];
+        for(int i = 0;i<num.length;i++){
+            System.out.println(i);
+        }
+    }
+}
