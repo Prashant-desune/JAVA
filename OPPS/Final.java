d2a1ab38fef56179aa94943a8e8b4dd8f5a9422b
Prashant Kumar <prashant.desune@gmail.com>
2022-05-08 22:38:12 +0530

    Add files via upload

---
+++ OPPS/Final.java
@@ -0,0 +1,31 @@
+class Test {
+    public final void show()
+    {
+        System.out.println();
+    }
+}
+
+class Test1 extends Test
+{
+    //public void show(){}
+    //gives error as final methods cannot be overloaded.
+}
+
+public class Final {
+
+    /*final float PI;
+    
+    public FinalDemo()
+    {
+        PI=3.1425f;
+    }*/
+    
+    public static void main(String[] args) {
+        
+        /*final float PI;
+        PI=3.1425f;
+        System.out.println(PI);*/
+        
+    }
+    
+}
